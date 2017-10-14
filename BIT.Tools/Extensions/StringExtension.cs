3961ec705f7805a67fc4bcf96d946232316795cb
jgabrielmc <jgabrielmc@gmail.com>
2017-10-14 02:44:05 -0500

    String extension

---
+++ BIT.Tools/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT.Tools.Extensions
+{
+    public static class StringExtension
+    {
+        public static String ToSafeString(this object val)
+        {
+            return (val ?? String.Empty).ToString();
+        }
+        public static string ToProperCase(this string text)
+        {
+            System.Globalization.CultureInfo cultureInfo = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Globalization.TextInfo textInfo = cultureInfo.TextInfo;
+            return textInfo.ToTitleCase(text);
+        }
+    }
+}
